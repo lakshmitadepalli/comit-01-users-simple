8adc2943dd6bfe77c00a926e758b569f1331d629
lakshmitadepalli <post2deepu6@gmail.com>
2023-04-17 19:27:32 -0600

    first commit

---
+++ src/main/java/com/comit/users/Comit01UsersSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.comit.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comit01UsersSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comit01UsersSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/users/Comit01UsersSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comit01UsersSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
